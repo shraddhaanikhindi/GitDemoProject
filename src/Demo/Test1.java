4451d81048acf534703bb03914de7461f84d4359
Admin <Admin@DESKTOP-J3CJJQI.Home>
2021-11-29 05:09:36 +0530

    first commit

---
+++ src/Demo/Test1.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "Yes";
+		
+		if (hungry == "yes")
+			System.out.println("eat something");
+		else {
+			System.out.println("Work");
+			
+		}
+	}
+
+}
