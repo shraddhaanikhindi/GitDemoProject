5f682247fb4c1b72183b0552e4415c0ab704b914
Shraddha A <shraddha.a.anikhindi@gmail.com>
2021-11-29 06:12:02 +0530

    Sel cucumber prj

--- src/Demo/Test1.java
+++ src/Demo/Test1.java
@@ -5,9 +5,13 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+	   //SDFF
+		
+		//dfdff
+		
 		String hungry = "Yes";
 		
-		if (hungry == "yes")
+		if (hungry == "no")
 			System.out.println("eat something");
 		else {
 			System.out.println("Work");
---
+++ src/Demo/Test2.java
@@ -0,0 +1,22 @@
+package Demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	   //SDFF
+		
+		//dfdff
+		
+		String hungry = "Yes";
+		
+		if (hungry == "yes")
+			System.out.println("eat something");
+		else {
+			System.out.println("Work");
+			
+		}
+	}
+
+}
